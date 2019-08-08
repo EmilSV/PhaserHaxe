28f869c5cbcd24c87a7246f0db767ea5c4871a83
Emil Schnedler Vad <emilsvad@gmail.com>
2019-08-08 23:17:32 +0200

    Change IDepth to use the new macro Mixin

--- src/phaserHaxe/gameobjects/components/Depth.hx
+++ src/phaserHaxe/gameobjects/components/Depth.hx
@@ -1,14 +1,7 @@
 package phaserHaxe.gameobjects.components;
 
-#if eval
-import haxe.macro.Context;
-import haxe.macro.Expr;
-#end
-
-using Lambda;
-
 @:allow(phaserHaxe.gameobjects.components.DepthImplementation)
-@:autoBuild(phaserHaxe.gameobjects.components.DepthBuilder.build())
+@:autoBuild(phaserHaxe.macro.Mixin.build(DepthMixin))
 interface IDepth
 {
 	/**
@@ -73,114 +66,68 @@ final class DepthImplementation
 	}
 
 	@:generic
-	public static inline function setDepth<T:IDepth>(self:T, value:Int = 0):T
+	public static inline function setDepth<T:IDepth>(self:T, value:Int):T
 	{
 		self.depth = value;
 		return self;
 	}
 }
 
-final class DepthBuilder
+@:noCompletion
+@:PhaserHaxe.NoMixin
+final class DepthMixin implements IDepth
 {
-	#if eval
-	public static function build():Array<Field>
+	/**
+	 * Private internal value. Holds the depth of the Game Object.
+	 *
+	 * @since 1.0.0
+	**/
+	private var _depth:Int;
+
+	/**
+	 * The depth of this Game Object within the Scene.
+	 *
+	 * The depth is also known as the 'z-index' in some environments, and allows you to change the rendering order
+	 * of Game Objects, without actually moving their position in the display list.
+	 *
+	 * The depth starts from zero (the default value) and increases from that point. A Game Object with a higher depth
+	 * value will always render in front of one with a lower value.
+	 *
+	 * Setting the depth will queue a depth sort event within the Scene.
+	 *
+	 * @since 1.0.0
+	**/
+	public var depth(get, set):Int;
+
+	private inline function get_depth():Int
 	{
-		// get existing fields from the context from where build() is called
-		final fields = Context.getBuildFields();
-		final pos = Context.currentPos();
-		final buildType = Context.toComplexType(Context.getLocalType());
-		final newFelids:Array<Field> = [];
-
-		function addField(field:Field)
-		{
-			newFelids.push(field);
-		}
-
-		addField({
-			name: "_depth",
-			doc: "Private internal value. Holds the depth of the Game Object.",
-			access: [APrivate],
-			kind: FVar(macro:Int, macro 0),
-			pos: pos
-		});
-
-		addField({
-			name: "depth",
-
-			doc: "The depth of this Game Object within the Scene.
-            The depth is also known as the 'z-index' in some environments, and allows you to change the rendering order 
-            of Game Objects, without actually moving their position in the display list.
-            
-            The depth starts from zero (the default value) and increases from that point. A Game Object with a higher depth 
-            value will always render in front of one with a lower value.
-            
-            Setting the depth will queue a depth sort event within the Scene.",
-
-			access: [APublic],
-			kind: FProp("get", "set", macro:Int),
-			pos: pos
-		});
-
-		addField({
-			name: "get_depth",
-			access: [APrivate, AInline],
-			kind: FFun({
-				args: [],
-				ret: macro:Int,
-				expr: macro return
-					phaserHaxe.gameobjects.components.DepthImplementation.get_depth(this)
-			}),
-			pos: pos
-		});
-
-		addField({
-			name: "set_depth",
-			access: [APrivate, AInline],
-			kind: FFun({
-				args: [
-					{
-						name: "value",
-						type: macro:Int
-					}
-				],
-				ret: macro:Int,
-				expr: macro return
-					phaserHaxe.gameobjects.components.DepthImplementation.set_depth(this, value)
-			}),
-			pos: pos
-		});
-
-		addField({
-			name: "setDepth",
-			doc: "The depth of this Game Object within the Scene.
-            
-            The depth is also known as the 'z-index' in some environments, and allows you to change the rendering order
-            of Game Objects, without actually moving their position in the display list.
-            The depth starts from zero (the default value) and increases from that point. A Game Object with a higher depth 
-            value will always render in front of one with a lower value. 
-            Setting the depth will queue a depth sort event within the Scene.
-            
-            @param value - The depth of this Game Object.
-            @return This Game Object instance.",
-
-			access: [APublic, AInline],
-			kind: FFun({
-				args: [
-					{
-						name: "value",
-						value: macro 0,
-						type: macro:Int
-					}
-				],
-				ret: buildType,
-				expr: macro return
-					phaserHaxe.gameobjects.components.DepthImplementation.setDepth(this, value)
-			}),
-			pos: pos
-		});
-
-		return newFelids.filter((f1) -> !fields.exists((f2) -> f1.name == f2.name))
-			.concat(fields);
+		return DepthImplementation.get_depth(this);
 	}
-	#end
+
+	private inline function set_depth(value:Int):Int
+	{
+		return DepthImplementation.set_depth(this, value);
+	}
+
+	/**
+	 * The depth of this Game Object within the Scene.
+	 *
+	 * The depth is also known as the 'z-index' in some environments, and allows you to change the rendering order
+	 * of Game Objects, without actually moving their position in the display list.
+	 *
+	 * The depth starts from zero (the default value) and increases from that point. A Game Object with a higher depth
+	 * value will always render in front of one with a lower value.
+	 *
+	 * Setting the depth will queue a depth sort event within the Scene.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param value - The depth of this Game Object.
+	 *
+	 * @return This Game Object instance.
+	**/
+	public inline function setDepth(value:Int = 0):DepthMixin
+	{
+		return DepthImplementation.setDepth(this, value);
+	}
 }
