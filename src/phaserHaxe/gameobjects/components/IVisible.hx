7724eb225946bd2e33222b76c6091e9649ac9345
Emil Schnedler Vad <emilsvad@gmail.com>
2019-08-12 19:25:31 +0200

    added IVisible

--- src/phaserHaxe/gameobjects/components/IVisible.hx
+++ src/phaserHaxe/gameobjects/components/IVisible.hx
@@ -1,5 +1,6 @@
 package phaserHaxe.gameobjects.components;
 
+@:allow(phaserHaxe.gameobjects.components.VisibleImplementation)
 interface IVisible
 {
 	/**
@@ -31,4 +32,82 @@ interface IVisible
 	 * @return This Game Object instance.
 	**/
 	public function setVisible(value:Bool):IVisible;
+}
+
+final class VisibleImplementation
+{
+	private static inline var _FLAG = 1;
+
+	public static inline function get_visible<T:IVisible>(self:T):Bool
+	{
+		return self._visible;
+	}
+
+	public static inline function set_visible<T:IVisible & GameObject>(self:T,
+			value:Bool):Bool
+	{
+		if (value)
+		{
+			self._visible = true;
+			self.renderFlags |= _FLAG;
+		}
+		else
+		{
+			self._visible = false;
+			self.renderFlags &= ~_FLAG;
+		}
+		return value;
+	}
+
+	public static inline function setVisible<T:IVisible & GameObject>(self:T,
+			value:Bool):T
+	{
+		self.visible = value;
+		return self;
+	}
+}
+
+final class VisibleMixin extends GameObject implements IVisible
+{
+	/**
+	 * Private internal value. Holds the visible value.
+	 *
+	 * @since 1.0.0
+	**/
+	private var _visible:Bool = true;
+
+	/**
+	 * The visible state of the Game Object.
+	 *
+	 * An invisible Game Object will skip rendering, but will still process update logic.
+	 *
+	 * @since 1.0.0
+	**/
+	public var visible(get, set):Bool;
+
+	private inline function get_visible():Bool
+	{
+		return VisibleImplementation.get_visible(this);
+	}
+
+	private function set_visible(value:Bool):Bool
+	{
+		return VisibleImplementation.set_visible(this, value);
+	}
+
+	/**
+	 * Sets the visibility of this Game Object.
+	 *
+	 * An invisible Game Object will skip rendering, but will still process update logic.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param value - The visible state of the Game Object.
+	 *
+	 * @return This Game Object instance.
+	**/
+	public function setVisible(value:Bool):VisibleMixin
+	{
+		return VisibleImplementation.setVisible(this, value);
+	}
 }
