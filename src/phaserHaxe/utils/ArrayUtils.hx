5fe62c74a0d9a8911fdce135aa633aec6ca5553b
Emil Schnedler Vad <emilsvad@gmail.com>
2019-09-10 22:38:04 +0200

    added more functions to ArrayUtils

--- src/phaserHaxe/utils/ArrayUtils.hx
+++ src/phaserHaxe/utils/ArrayUtils.hx
@@ -333,6 +333,255 @@ final class ArrayUtils
 	}
 
 	/**
+	 * Passes each element in the array, between the start and end indexes, to the given callback.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array - The array to search.
+	 * @param callback - A callback to be invoked for each item in the array.
+	 * @param context - The context in which the callback is invoked.
+	 * @param startIndex - The start index to search from.
+	 * @param endIndex - The end index to search to.
+	 * @param args - Additional arguments that will be passed to the callback, after the child.
+	 *
+	 * @return The input array.
+	**/
+	public static function eachInRange<T>(array:Array<T>, callback:Function,
+			context:Dynamic, startIndex:Int = 0, ?endIndex:Int, ?args:Array<Dynamic>)
+	{
+		final endIndex:Int = endIndex != null ? endIndex : array.length;
+
+		if (safeRange(array, startIndex, endIndex))
+		{
+			var args = args.copy();
+			args.insert(0, null);
+
+			for (i in startIndex...endIndex)
+			{
+				args[0] = array[i];
+
+				Reflect.callMethod(context, callback, args);
+			}
+		}
+
+		return array;
+	}
+
+	/**
+	 * Searches a pre-sorted array for the property with the closet value to the given number.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param value - The value to search for in the array.
+	 * @param array - The array to search, which must be sorted.
+	 * @param propertyAccessor - The The function to get the property to search by.
+	 *
+	 * @return The nearest value found in the array, or if a `key` was given, the nearest object with the matching property value.
+	**/
+	public static inline function findClosestInSorted<T>(value:Float, array:Array<T>,
+			propertyAccessor:(T) -> Float):Float
+	{
+		if (array.length == 0)
+		{
+			return Math.NaN;
+		}
+		else if (array.length == 1)
+		{
+			return propertyAccessor(array[0]);
+		}
+		var i = 1, low:Float, high:Float;
+
+		if (value < propertyAccessor(array[0]))
+		{
+			return propertyAccessor(array[0]);
+		}
+		while (propertyAccessor(array[i]) < value)
+		{
+			i++;
+		}
+
+		if (i > array.length)
+		{
+			i = array.length;
+		}
+
+		low = propertyAccessor(array[i - 1]);
+		high = propertyAccessor(array[i]);
+		return
+			((high - value) <= (value - low)) ? propertyAccessor(array[i]) : propertyAccessor(array[i - 1]);
+	}
+
+	/**
+	 * Returns all elements in the array.
+	 *
+	 * You can optionally specify a matching criteria using the `propertyAccessor` and `value` arguments.
+	 *
+	 * For example: `getAll(i -> i.visible , true)` would return only elements that have their visible property set to true.
+	 *
+	 * Optionally you can specify a start and end index. For example if the array had 100 elements,
+	 * and you set `startIndex` to 0 and `endIndex` to 50, it would return matches from only
+	 * the first 50 elements.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array - The array to search.
+	 * @param propertyAccessor - The function to get the property to test on each array element.
+	 * @param value - The value to test the property against.
+	 * @param startIndex - An optional start index to search from.
+	 * @param endIndex - An optional end index to search to.
+	 *
+	 * @return All matching elements from the array.
+	**/
+	public static inline function getAll<T1, T2>(array:Array<T1>,
+			?propertyAccessor:(T1) -> T2, ?value:T2, startIndex:Int = 0,
+			?endIndex:Int):Array<T1>
+	{
+		final endIndex:Int = endIndex != null ? endIndex : array.length;
+		var output = [];
+
+		if (safeRange(array, startIndex, endIndex))
+		{
+			for (i in startIndex...endIndex)
+			{
+				final child = array[i];
+				if (propertyAccessor == null || (value != null && propertyAccessor(child) == value))
+				{
+					output.push(child);
+				}
+			}
+		}
+		return output;
+	}
+
+	/**
+	 * Returns the first element in the array.
+	 *
+	 * You can optionally specify a matching criteria using the `propertyAccessor` and `value` arguments.
+	 *
+	 * For example: `getAll(i -> i.visible , true)` would return the first element that had its `visible` property set to true.
+	 *
+	 * Optionally you can specify a start and end index. For example if the array had 100 elements,
+	 * and you set `startIndex` to 0 and `endIndex` to 50, it would search only the first 50 elements.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array - The array to search.
+	 * @param propertyAccessor -The function to get the property to test on each array element.
+	 * @param value - The value to test the property against.
+	 * @param startIndex - An optional start index to search from.
+	 * @param endIndex - An optional end index to search up to (but not included)
+	 *
+	 * @return The first matching element from the array, or `null` if no element could be found in the range given.
+	**/
+	public static inline function getFirst<T1, T2>(array:Array<T1>,
+			?propertyAccessor:(T1) -> T2, ?value:T2, startIndex:Int = 0,
+			?endIndex:Int):Null<T1>
+	{
+		final endIndex:Int = endIndex != null ? endIndex : array.length;
+
+		if (safeRange(array, startIndex, endIndex))
+		{
+			for (i in startIndex...endIndex)
+			{
+				final child = array[i];
+				if (propertyAccessor == null || (value != null && propertyAccessor(child) == value))
+				{
+					return child;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns a Random element from the array.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array - The array to select the random entry from.
+	 * @param startIndex - An optional start index.
+	 * @param length - An optional length, the total number of elements (from the startIndex) to choose from.
+	 *
+	 * @return A random element from the array, or `null` if no element could be found in the range given.
+	**/
+	public static function getRandom<T>(array:Array<T>, startIndex:Int = 0,
+			?length:Int):Null<T>
+	{
+		final length:Int = length != null ? length : array.length;
+
+		var randomIndex = startIndex + Math.floor(Math.random() * length);
+
+		if (randomIndex >= 0 && randomIndex < array.length)
+		{
+			return array[randomIndex];
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * Moves the given array element down one place in the array.
+	 * The array is modified in-place.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array - The input array.
+	 * @param item - The element to move down the array.
+	 *
+	 * @return The input array.
+	**/
+	public static function moveDown<T>(array:Array<T>, item:T):Array<T>
+	{
+		var currentIndex = array.indexOf(item);
+
+		if (currentIndex > 0)
+		{
+			var index2 = currentIndex - 1;
+
+			var item2 = array[index2];
+
+			array[currentIndex] = item2;
+			array[index2] = item;
+		}
+
+		return array;
+	}
+
+	/**
+	 * Moves an element in an array to a new position within the same array.
+	 * The array is modified in-place.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array - The array.
+	 * @param item - The element to move.
+	 * @param index - The new index that the element will be moved to.
+	 *
+	 * @return The element that was moved.
+	**/
+	public static function moveTo<T>(array:Array<T>, item:T, index:Int):T
+	{
+		var currentIndex = array.indexOf(item);
+
+		if (currentIndex == -1 || index < 0 || index >= array.length)
+		{
+			throw new Error('Supplied index out of bounds');
+		}
+
+		if (currentIndex != index)
+		{
+			//  Remove
+			array.splice(currentIndex, 1);
+
+			//  Add in new location
+			array.insert(index, item);
+		}
+		return item;
+	}
+
+	/**
 	 * Tests if the start and end indexes are a safe range for the given array.
 	 *
 	 * @since 1.0.0
